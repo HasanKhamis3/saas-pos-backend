a436a277025f895de000b5d7dc9fc0b3cb9950fa
HasanKhamis3 <hshmk3@gmail.com>
2026-05-15 20:00:30 +0000

    Sync

--- api/src/modules/pos/controllers/decorators/roles.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRole } from '../entities/user.entity';
-
-export const ROLES_KEY = 'roles';
-// هذه الدالة تسمح لنا بتحديد من المسموح له بالدخول (مثلاً: Roles(UserRole.MANAGER))
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
